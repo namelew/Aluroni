e4d3cefd9097d8dda47b75066e136a217a75b265
namelew <diogomaciel.cunha@gmail.com>
2023-07-21 18:41:09 -0300

    Adicionando pacotes e criando primeira página e componente
    Adicionando os pacotes typescript-plugin-css-modules e sass para gerenciar
    estilos e criando o primeiro componente, Button, e a primeira página, Menu

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,9 @@
+import styles from "./Button.module.scss"
+
+const Button = () => {
+    return (
+        <button className={styles.button}>Botão</button>
+    )
+}
+
+export default Button;
---
+++ src/pages/Menu/index.tsx
@@ -0,0 +1,13 @@
+import styles from './Menu.module.scss'
+
+const Menu = () => {
+    return (
+        <main>
+            <nav className={styles.menu}>
+
+            </nav>
+        </main>
+    )
+}
+
+export default Menu;
