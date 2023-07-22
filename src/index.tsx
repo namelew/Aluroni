aa9b4fa55afcd0f197b9b0d96f4f1dfec188ad85
namelew <diogomaciel.cunha@gmail.com>
2023-07-22 12:02:20 -0300

    Importando font do projeto e normalize.css

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Menu from './pages/Menu';
+import 'normalize.css';
 import './index.css';
 
 const root = ReactDOM.createRoot(
