5a85a3f84fdc898474b830d20c75488768394e2c
namelew <diogomaciel.cunha@gmail.com>
2023-07-22 11:51:40 -0300

    Criando Footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {ReactComponent as Logo} from '/assets/logo.svg';
+
+const Footer = () => {
+ return (
+   <footer>
+     <h2>Aluraflix</h2>
+     <p>Veja filmes a qualquer hora!</p>
+     <Logo/>
+   </footer>
+ );
+}
+
+export default Footer;
