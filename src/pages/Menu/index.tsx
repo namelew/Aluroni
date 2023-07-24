14ddca6d35300a40e07b9eff5b69b5f3d7082bf4
namelew <diogomaciel.cunha@gmail.com>
2023-07-24 09:35:49 -0300

    Movendo para pasta components

--- src/pages/Menu/index.tsx
+++ src/pages/Menu/index.tsx
@@ -3,7 +3,7 @@ import { useState } from 'react';
 import styles from './Menu.module.scss';
 import { ReactComponent as Logo} from 'assets/logo.svg';
 import Searcher from './Searcher';
-import Filters from './Filters';
+import Filters from 'components/Filters';
 
 const Menu = () => {
     const [search, setSearch] = useState('');
