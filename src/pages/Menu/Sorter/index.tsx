a1d771e4599b846b6cd3986e9078cebeced033dd
namelew <diogomaciel.cunha@gmail.com>
2023-07-24 10:20:51 -0300

    Adicionando comportamento e icones no sorter

--- src/pages/Menu/Sorter/index.tsx
+++ src/pages/Menu/Sorter/index.tsx
@@ -1,13 +1,26 @@
+import { useState } from 'react';
+import { MdKeyboardArrowUp, MdKeyboardArrowDown } from 'react-icons/md';
+import classNames from 'classnames';
 import styles from './Sorter.module.scss';
 import options from './options.json';
 
 type IOptions = typeof options[0];
 
 const Sorter = () => {
+    const [open, setOpen] = useState(false);
+
     return (
-        <button className={styles.sorter}>
+        <button
+            className={styles.sorter}
+            onClick={ _ => setOpen(!open) }
+            onBlur={ _ => setOpen(false) }
+        >
             <span>Ordenar por</span>
-            <div className={styles.sorter__options}>
+            {open ? <MdKeyboardArrowUp size={20}/> : <MdKeyboardArrowDown size={20}/>}
+            <div className={classNames({
+                [styles.sorter__options]: true,
+                [styles['sorter__options--active']]: open,
+            })}>
                 {options.map(option => (
                     <div className={styles.sorter__option} key={option.value}>
                         {option.name}
