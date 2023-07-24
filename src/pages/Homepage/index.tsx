d4d68f246be62b8333d4e94a4dab07a374619ba7
namelew <diogomaciel.cunha@gmail.com>
2023-07-24 14:30:16 -0300

    Criando página inicial do site

---
+++ src/pages/Homepage/index.tsx
@@ -0,0 +1,10 @@
+const Homepage = () => {
+    return (
+        <section>
+            <h3>Recomendações da cozinha</h3>
+            <div></div>
+        </section>
+    );
+}
+
+export default Homepage;
