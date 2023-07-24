e6c53c0291e83b94df87396b2a98525f67e6c84b
namelew <diogomaciel.cunha@gmail.com>
2023-07-24 11:43:22 -0300

    Adicionando imagens aos pratos

--- src/pages/Menu/List/Item/index.tsx
+++ src/pages/Menu/List/Item/index.tsx
@@ -1,14 +1,14 @@
-import logo from 'assets/logo.svg';
+import classNames from 'classnames';
 import styles from './Item.module.scss';
 import items from '../itens.json';
 
 type Props = typeof items[0];
 
-const Item = ( { title, description, category, size, serving, price } : Props) => {
+const Item = ( { title, description, category, size, serving, price, photo } : Props) => {
     return (
         <div className={styles.item}>
             <div className={styles.item__image}>
-                <img src={logo} alt={title} />
+                <img src={photo} alt={title} />
             </div>
             <div className={styles.item__description}>
                 <div className={styles.item__title}>
@@ -16,7 +16,10 @@ const Item = ( { title, description, category, size, serving, price } : Props) =
                     <p> {description} </p>
                 </div>
                 <div className={styles.item__tags}>
-                    <div className={styles.item__type}>
+                    <div className={classNames({
+                        [styles.item__type]: true,
+                        [styles[`item__type__${category.label.toLowerCase()}`]]: true,
+                    })}>
                         {category.label}
                     </div>
                     <div className={styles.item__potion}>
