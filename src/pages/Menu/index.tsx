a4e2a531af7f175836063198a6130368ad12b1ec
namelew <diogomaciel.cunha@gmail.com>
2023-07-24 08:13:36 -0300

    Criando header e adicionando arquivo de variáveis scss

--- src/pages/Menu/index.tsx
+++ src/pages/Menu/index.tsx
@@ -7,6 +7,11 @@ const Menu = () => {
             <nav className={styles.menu}>
                 <Logo />
             </nav>
+            <header className={styles.header}>
+                <div className={styles.header__text}>
+                    A casa do código e da massa
+                </div>
+            </header>
         </main>
     )
 }
