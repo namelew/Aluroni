0cf4387ec6110e43781c51a74810826f290eea2b
namelew <diogomaciel.cunha@gmail.com>
2023-08-17 06:39:01 -0300

    Adicionando header no router

--- src/pages/FoodList/index.tsx
+++ src/pages/FoodList/index.tsx
@@ -1,4 +1,3 @@
-import Header from 'components/Header';
 import { useState } from 'react';
 import styles from './FoodList.module.scss';
 import Searcher from './Searcher';
@@ -12,19 +11,15 @@ const FoodList = () => {
     const [sorter, setSorter] = useState<SortOptions>('');
 
     return (
-        <main>
-            <Header>A casa do código e da massa</Header>
-
-            <section className={styles.billOfFare}>
-                <h3 className={styles.billOfFare__title}>Cardápio</h3>
-                <Searcher search={search} setSearch={setSearch}/>
-                <div className={styles.billOfFare__filters}>
-                    <Filters filter={filter} setFilter={setFilter}/>
-                    <Sorter sorter={sorter} setSorter={setSorter}/>
-                </div>
-                <List search={search} filter={filter} sorter={sorter}/>
-            </section>
-        </main>
+        <section className={styles.billOfFare}>
+            <h3 className={styles.billOfFare__title}>Cardápio</h3>
+            <Searcher search={search} setSearch={setSearch}/>
+            <div className={styles.billOfFare__filters}>
+                <Filters filter={filter} setFilter={setFilter}/>
+                <Sorter sorter={sorter} setSorter={setSorter}/>
+            </div>
+            <List search={search} filter={filter} sorter={sorter}/>
+        </section>
     );
 };
 
--- src/router.js
+++ src/router.js
@@ -2,16 +2,20 @@ import Homepage from 'pages/Homepage';
 import FoodList from 'pages/FoodList';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Menu from 'components/Menu';
+import Header from 'components/Header';
 
 const AppRouter = () => {
     return (
-        <Router>
-            <Menu/>
-            <Routes>
-                <Route path='/' element={<Homepage/>}/>
-                <Route path='/cardapio' element={<FoodList/>}/>
-            </Routes>
-        </Router>
+        <main>
+            <Router>
+                <Menu/>
+                <Header>A casa do código e da massa</Header>
+                <Routes>
+                    <Route path='/' element={<Homepage/>}/>
+                    <Route path='/cardapio' element={<FoodList/>}/>
+                </Routes>
+            </Router>
+        </main>
     );
 };
 
