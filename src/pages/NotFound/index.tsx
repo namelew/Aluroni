5e768a5197cb749f7024ccec2d1ecf0d8f873251
namelew <diogomaciel.cunha@gmail.com>
2023-08-17 11:29:50 -0300

    Criando botão voltar

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -2,13 +2,21 @@ import styles from './NotFound.module.scss';
 import tema from 'styles/Tema.module.scss';
 import { ReactComponent as NotFoundImg } from 'assets/not_found.svg';
 import classNames from 'classnames';
+import { useNavigate } from 'react-router-dom';
 
 const NotFound = () => {
+    const navigate = useNavigate();
+
     return (
         <div className={classNames({
             [styles.container]: true,
             [tema.container]: true,
         })}>
+            <div className={styles.back}>
+                <button onClick={() => { navigate(-1); }}>
+                    {'< Voltar'}
+                </button>
+            </div>
             <NotFoundImg />
         </div>
     );
