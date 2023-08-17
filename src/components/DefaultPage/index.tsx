a3db5bfac3a73be9702f9441dc31ca8212d9038f
namelew <diogomaciel.cunha@gmail.com>
2023-08-17 08:20:59 -0300

    removendo component de header

--- src/components/DefaultPage/index.tsx
+++ src/components/DefaultPage/index.tsx
@@ -1,10 +1,14 @@
-import Header from 'components/Header';
 import { Outlet } from 'react-router-dom';
+import styles from './DefaultPage.module.scss';
 
 const DefaultPage = () => {
     return (
         <>
-            <Header>A casa do código e da massa</Header>
+            <header className={styles.header}>
+                <div className={styles.header__text}>
+                    A casa do código e da massa
+                </div>
+            </header>
             <div>
                 <Outlet />
             </div>
