6c559310a2bfc7ee9bb80e3605658b2f41acfda7
namelew <diogomaciel.cunha@gmail.com>
2023-08-17 08:28:04 -0300

    Criando tema padrão para a aplicação

--- src/components/DefaultPage/index.tsx
+++ src/components/DefaultPage/index.tsx
@@ -1,5 +1,6 @@
 import { Outlet } from 'react-router-dom';
 import styles from './DefaultPage.module.scss';
+import tema from 'styles/Tema.module.scss';
 
 const DefaultPage = () => {
     return (
@@ -9,7 +10,7 @@ const DefaultPage = () => {
                     A casa do código e da massa
                 </div>
             </header>
-            <div>
+            <div className={tema.container}>
                 <Outlet />
             </div>
         </>
--- src/pages/Homepage/index.tsx
+++ src/pages/Homepage/index.tsx
@@ -1,5 +1,6 @@
 import menu from 'data/bill-of-fare.json';
 import styles from './Homepage.module.scss';
+import tema from 'styles/Tema.module.scss';
 
 const Homepage = () => {
     const recommendations = [...menu].sort(() => 0.5 - Math.random()).splice(0,3);
@@ -7,7 +8,7 @@ const Homepage = () => {
 
     return (
         <section>
-            <h3 className={styles.title}>Recomendações da cozinha</h3>
+            <h3 className={tema.title}>Recomendações da cozinha</h3>
             <div className={styles.recommendations}>
                 {recommendations.map(item => (
                     <div key={item.id} className={styles.recommendation}>
